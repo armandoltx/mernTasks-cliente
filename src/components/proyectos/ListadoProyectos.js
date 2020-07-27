973ac05e86c99e04874e5ad8278cc049a2fa11a5
Armando Ruiz <armando.ruiz@industrie.co>
2020-07-27 10:32:15 +1000

    203 Agregando los proyectos al state y pasandolos al listado con context

--- src/components/proyectos/ListadoProyectos.js
+++ src/components/proyectos/ListadoProyectos.js
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Proyecto from './Proyecto';
+import proyectoContext from '../../context/proyectos/proyectoContext';
 
 const ListadoProyectos = () => {
 
-  const proyectos = [
-    {nombre: 'Tienda Virtual'},
-    {nombre: 'Intranet'},
-    {nombre: 'Sitio Web'}
-  ]
+  //Extraer proyectos del state inicial
+  // primero definimos el context asi podemos consumir el state formulario sin necesidad de pasarlo a lo largo del arbol de componentes. Hay q pasar el context como parametro para acceder a el.
+  const proyectosContext = useContext(proyectoContext);
+  const { proyectos } = proyectosContext;
+
+  // Revisar si proyectos tiene contenido
+  if(proyectos.length ===0) return null;
 
   return (
     <ul className="listado-proyectos">
       {proyectos.map(proyecto => (
-        <Proyecto proyecto={proyecto}/>
+        <Proyecto
+          key={proyecto.id}
+          proyecto={proyecto}
+        />
       ))}
     </ul>
   );
